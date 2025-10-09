f6dc90e69557df1ac3c790532582a6fe8878041e
Aluno.Etec <aula.etec.com>
2025-10-09 20:42:20 -0300

    tabela verdade

--- janela.js
+++ janela.js
@@ -1,6 +1,7 @@
 //var nome= prompt("digite seu nome ")
 //alert("seu nome é: "+nome)
-var a= prompt("digite o primeiro numero: ") 
-var b = prompt("figite o segundo numero: ")
-var c= prompt("digite o terceiro numero: ")
+var a= prompt(Number("digite o primeiro numero: "));
+var b = prompt(Number("figite o segundo numero: "));
+var c= prompt(Number("digite o terceiro numero: "));
+var soma= ( a+b+c);
 alert(a+b+c)
